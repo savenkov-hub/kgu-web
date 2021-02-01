dabef83d8b4e4936bb5241f85e7fc0bf314b272e
savenkov-hub <66920140+savenkov-hub@users.noreply.github.com>
2021-02-01 19:21:58 +0500

    testing

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "User page: " + name + " - " + str(id)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
