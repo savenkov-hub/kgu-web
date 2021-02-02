cfb8bb051ff207fed94a2626be2b0f0f567a065c
savenkov-hub <66920140+savenkov-hub@users.noreply.github.com>
2021-02-02 22:31:19 +0500

    хз, пока не получается

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, url_for, request, redirect
+import os
+from flask import Flask, render_template, url_for, request, redirect, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/static/images'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kguweb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
+
 
 
 class Article(db.Model):
@@ -14,12 +21,13 @@ class Article(db.Model):
     intro = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
+    file = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return '<Article %r>' % self.id
 
 
-#Вызов старой главной, не активна, оставим на всякий случай
+#Вызов старой главной, не используется, оставил на всякий случай
 @app.route('/home')
 def index():
     return render_template("index.html")
@@ -40,6 +48,11 @@ def post_detail(id):
     return render_template("post_detail.html", article=article)
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
 #Вызов метода\страницы добавления поста
 @app.route('/creat-article', methods=['POST', 'GET'])
 def creat_article():
@@ -47,12 +60,16 @@ def creat_article():
         title = request.form['title']
         intro = request.form['intro']
         text = request.form['text']
+        file = request.files['file']
 
-        article = Article(title=title, intro=intro, text=text)
+        article = Article(title=title, intro=intro, text=text, file=file)
 
         try:
-            db.session.add(article)
-            db.session.commit()
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                db.session.add(article)
+                db.session.commit()
             return redirect('/posts')
         except:
             return "При добавлении статьи произошла ошибка"
@@ -90,6 +107,10 @@ def post_update(id):
     else:
         return render_template("post_update.html", article=article)
 
+
+@app.route('/images/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == '__main__':
